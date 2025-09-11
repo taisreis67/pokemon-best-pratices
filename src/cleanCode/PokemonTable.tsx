651b96d3bed82b23b5b096d5ac043e76fa39ee1b
Tais Duarte <taisrsduarte@gmail.com>
2025-09-11 20:58:17 -0300

    feat: apply clean code

---
+++ src/cleanCode/PokemonTable.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import type { PokemonInfos } from './types/PokemonInfos'
+import PokemonTableRow from './PokemonTableRow'
+
+const PokemonTable: React.FC<{ pokemon: PokemonInfos }> = ({ pokemon }) => (
+  <table className="w-full mt-4 border-collapse">
+    <thead>
+      <tr className="bg-gray-200">
+        <th className="p-2">Nome</th>
+        <th className="p-2">ID</th>
+        <th className="p-2">Altura</th>
+        <th className="p-2">Peso</th>
+        <th className="p-2">Imagem</th>
+      </tr>
+    </thead>
+    <tbody>
+      <PokemonTableRow pokemon={pokemon} />
+    </tbody>
+  </table>
+)
+
+export default PokemonTable
